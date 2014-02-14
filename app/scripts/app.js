0551afd461ddb08464b65631d23316042767e8f0
Andri Mar Björgvinsson <andri@sprettur.is>
2014-02-14 17:54:48 +0000

    First session finished in leanisland

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,4 +1,25 @@
 'use strict';
 
-angular.module('agileisland.isApp', []).config(function () {
+angular.module('leanisland.isApp', ['ngRoute']).config(function ($routeProvider) {
+	$routeProvider
+        .when('/', {
+            templateUrl: 'views/main.html',
+            controller: 'MainCtrl'
+        })
+        .when('/skraning', {
+            templateUrl: 'views/skraning.html',
+            controller: 'MainCtrl'
+        })
+        .when('/dagskra', {
+            templateUrl: 'views/dagskra.html',
+            controller: 'MainCtrl'
+        })
+        .when('/namskeid', {
+            templateUrl: 'views/namskeid.html',
+            controller: 'MainCtrl'
+        })
+        .when('/um-opid-rymi', {
+            templateUrl: 'views/um_opid_rymi.html',
+            controller: 'MainCtrl'
+        })
 });
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('agileisland.isApp')
+angular.module('leanisland.isApp')
   .controller('MainCtrl', function ($scope) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
